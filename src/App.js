12fd7e9bf18f17c7ad544b6bd88193c5993812ca
MaxGenest <maxime@olybe.com>
2021-06-11 16:28:24 +0200

    add styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
 import Canvas from './components/Canvas'
 
 function App() {
+  const headerRef = useRef(null);
+  const [canvasHeight, setCanvasHeight] = useState(0);
+
+  useEffect(() => {
+    if (headerRef?.current !== null) {
+      setCanvasHeight(window.innerHeight - headerRef.current.offsetHeight)
+    }
+  }, [headerRef])
+
   const draw = (ctx, frameCount) => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
     ctx.fillStyle = '#000000'
@@ -14,10 +24,16 @@ function App() {
 
   return (
     <div>
-      <h1>Web Art by Maxime</h1>
-      <Canvas draw={draw} options={options} />
+      <div ref={headerRef} id='header'>
+        <Title>Web Art by Maxime</Title>
+      </div>
+      <Canvas draw={draw} options={options} height={canvasHeight} width={window.innerWidth} />
     </div>
   );
 }
+
+const Title = styled.h1`
+  margin: 0;
+`
 
 export default App;
