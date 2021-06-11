f1d898c5841934398d93ad7dc6be85912e4c897b
MaxGenest <maxime@olybe.com>
2021-06-11 16:09:53 +0200

    base of react canvas working

--- src/hooks/useCanvas.js
+++ src/hooks/useCanvas.js
@@ -2,13 +2,32 @@ import { useRef, useEffect } from 'react'
 
 const useCanvas = (draw, options={}) => {
   const canvasRef = useRef(null)
+
+  const resizeCanvasToDisplaySize = (canvas) => {
+    const { width, height } = canvas.getBoundingClientRect()
+
+    if (canvas.width !== width || canvas.height !== height) {
+      const { devicePixelRatio:ratio=1 } = window
+      const context = canvas.getContext('2d')
+
+      canvas.width = width * ratio
+      canvas.height = height * ratio
+      context.scale(ratio, ratio)
+      return true // here you can return some usefull information like delta width and delta height instead of just true
+      // this information can be used in the next redraw...
+    }
+
+    return false
+  }
   
   useEffect(() => {
     const canvas = canvasRef.current
-    const context = canvas.getContext(options.context || '2d')
+    const context = canvas.getContext(options.context ?? '2d')
     let frameCount = 0
     let animationFrameId
     
+    resizeCanvasToDisplaySize(canvas)
+
     const render = () => {
       frameCount++
       draw(context, frameCount)
